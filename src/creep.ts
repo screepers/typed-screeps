2dbe469b3e9e9d796b50276f045c30840a6238dc
Isis McCafferty <isis@lotuswind.net>
2016-07-06 00:13:22 -0700

    more StoreDefinition fixes
    make `energy` optional
    add `power` back
    change `creep.carry` to `StoreDefinition`

--- src/creep.ts
+++ src/creep.ts
@@ -16,7 +16,7 @@ declare class Creep extends RoomObject{
      * energy: number
      * The current amount of energy the creep is carrying.
      */
-    carry: { [resource: string]: number };
+    carry: StoreDefinition;
     /**
      * The total amount of resources the creep can carry.
      */
