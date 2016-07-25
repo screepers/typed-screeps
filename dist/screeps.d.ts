83eff8dac4d2b44def78020b59c9aab8a18e80ab
Nhan Ho <human@nhanho.me>
2016-07-25 13:13:58 +0700

    Change Source to class instead of interface and extending RoomObject

--- dist/screeps.d.ts
+++ dist/screeps.d.ts
@@ -1773,7 +1773,7 @@ declare class Room {
 /**
  * An energy source object. Can be harvested by creeps with a WORK body part.
  */
-interface Source {
+declare class Source extends RoomObject {
     /**
      * The prototype is stored in the Source.prototype global object. You can use it to extend game objects behaviour globally:
      */
@@ -1790,14 +1790,6 @@ interface Source {
      * A unique object identificator. You can use Game.getObjectById method to retrieve an object instance by its id.
      */
     id: string;
-    /**
-     * An object representing the position of this structure in the room.
-     */
-    pos: RoomPosition;
-    /**
-     * The link to the Room object of this structure.
-     */
-    room: Room;
     /**
      * The remaining time after which the source will be refilled.
      */
