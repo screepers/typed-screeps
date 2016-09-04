ea9897b7af1213d21ae108c93826022d6630cb0e
Sean Lindsay <seancl4@gmail.com>
2016-09-03 18:39:18 -0700

    Add Nuke class
    Fixes issue #53

--- dist/screeps.d.ts
+++ dist/screeps.d.ts
@@ -1132,6 +1132,23 @@ interface Mineral extends RoomObject {
     ticksToRegeneration: number;
 }
 /**
+ * A nuke landing position. This object cannot be removed or modified. You can find incoming nukes in the room using the FIND_NUKES constant.
+ */
+declare class Nuke extends RoomObject {
+    /**
+     * A unique object identificator. You can use Game.getObjectById method to retrieve an object instance by its id.
+     */
+    id: string;
+    /**
+     * The name of the room where this nuke has been launched from.
+     */
+    launchRoomName: string;
+    /**
+     * The remaining landing time.
+     */
+    timeToLand: number;
+}
+/**
  * Contains powerful methods for pathfinding in the game world. Support exists for custom navigation costs and paths which span multiple rooms.
  * Additionally PathFinder can search for paths through rooms you can't see, although you won't be able to detect any dynamic obstacles like creeps or buildings.
  */
---
+++ src/nuke.ts
@@ -0,0 +1,17 @@
+/**
+ * A nuke landing position. This object cannot be removed or modified. You can find incoming nukes in the room using the FIND_NUKES constant.
+ */
+declare class Nuke extends RoomObject {
+    /**
+     * A unique object identificator. You can use Game.getObjectById method to retrieve an object instance by its id.
+     */
+    id: string;
+    /**
+     * The name of the room where this nuke has been launched from.
+     */
+    launchRoomName: string;
+    /**
+     * The remaining landing time.
+     */
+    timeToLand: number;
+}
