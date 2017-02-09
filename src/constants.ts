67f3831cb7f21a11a0bcb3bd99bb4d546185603a
Michael Søndergaard <sheeo@sheeo.dk>
2017-02-09 05:56:56 +0100

    Replace some of the constants with static types (#94)
    Makes for better type inference, since `const` declarations are inferred
    to their narrowest type at declaration.
    
    E.g. `const x = 5;` infers the type of `x` to be `5`, making the literal
    `5` compatible.
    
    Since types are generally widened without a problem, this shouldn't
    be an incompatible change with anyones codebase -- although number type
    literals are a relatively new typescript feature, so full utilization of
    this may require TS 2.1.

--- src/constants.ts
+++ src/constants.ts
@@ -3,240 +3,251 @@
  * This might need some updates when Screeps publishes new features or changes it's existing API
  */
 
-declare const FIND_EXIT_TOP: number;
-declare const FIND_EXIT_RIGHT: number;
-declare const FIND_EXIT_BOTTOM: number;
-declare const FIND_EXIT_LEFT: number;
-declare const FIND_EXIT: number;
-declare const FIND_CREEPS: number;
-declare const FIND_MY_CREEPS: number;
-declare const FIND_HOSTILE_CREEPS: number;
-declare const FIND_SOURCES_ACTIVE: number;
-declare const FIND_SOURCES: number;
-declare const FIND_DROPPED_RESOURCES: number;
-declare const FIND_DROPPED_ENERGY: number;
-declare const FIND_STRUCTURES: number;
-declare const FIND_MY_STRUCTURES: number;
-declare const FIND_HOSTILE_STRUCTURES: number;
-declare const FIND_FLAGS: number;
-declare const FIND_CONSTRUCTION_SITES: number;
-declare const FIND_MY_CONSTRUCTION_SITES: number;
-declare const FIND_HOSTILE_CONSTRUCTION_SITES: number;
-declare const FIND_MY_SPAWNS: number;
-declare const FIND_HOSTILE_SPAWNS: number;
-declare const FIND_MINERALS: number;
+declare const OK: 0;
+declare const ERR_NOT_OWNER: -1;
+declare const ERR_NO_PATH: -2;
+declare const ERR_NAME_EXISTS: -3;
+declare const ERR_BUSY: -4;
+declare const ERR_NOT_FOUND: -5;
+declare const ERR_NOT_ENOUGH_RESOURCES: -6;
+declare const ERR_NOT_ENOUGH_ENERGY: -6;
+declare const ERR_INVALID_TARGET: -7;
+declare const ERR_FULL: -8;
+declare const ERR_NOT_IN_RANGE: -9;
+declare const ERR_INVALID_ARGS: -10;
+declare const ERR_TIRED: -11;
+declare const ERR_NO_BODYPART: -12;
+declare const ERR_NOT_ENOUGH_EXTENSIONS: -6;
+declare const ERR_RCL_NOT_ENOUGH: -14;
+declare const ERR_GCL_NOT_ENOUGH: -15;
 
-declare const TOP: number;
-declare const TOP_RIGHT: number;
-declare const RIGHT: number;
-declare const BOTTOM_RIGHT: number;
-declare const BOTTOM: number;
-declare const BOTTOM_LEFT: number;
-declare const LEFT: number;
-declare const TOP_LEFT: number;
+declare const FIND_EXIT_TOP: 1;
+declare const FIND_EXIT_RIGHT: 3;
+declare const FIND_EXIT_BOTTOM: 5;
+declare const FIND_EXIT_LEFT: 7;
+declare const FIND_EXIT: 10;
+declare const FIND_CREEPS: 101;
+declare const FIND_MY_CREEPS: 102;
+declare const FIND_HOSTILE_CREEPS: 103;
+declare const FIND_SOURCES_ACTIVE: 104;
+declare const FIND_SOURCES: 105;
+declare const FIND_DROPPED_RESOURCES: 106;
+declare const FIND_DROPPED_ENERGY: 106; // Yup, it's 106.
+declare const FIND_STRUCTURES: 107;
+declare const FIND_MY_STRUCTURES: 108;
+declare const FIND_HOSTILE_STRUCTURES: 109;
+declare const FIND_FLAGS: 110;
+declare const FIND_CONSTRUCTION_SITES: 111;
+declare const FIND_MY_SPAWNS: 112;
+declare const FIND_HOSTILE_SPAWNS: 113;
+declare const FIND_MY_CONSTRUCTION_SITES: 114;
+declare const FIND_HOSTILE_CONSTRUCTION_SITES: 115;
+declare const FIND_MINERALS: 116;
+declare const FIND_NUKES: 117;
 
-declare const OK: number;
-declare const ERR_NOT_OWNER: number;
-declare const ERR_NO_PATH: number;
-declare const ERR_NAME_EXISTS: number;
-declare const ERR_BUSY: number;
-declare const ERR_NOT_FOUND: number;
-declare const ERR_NOT_ENOUGH_RESOURCES: number;
-declare const ERR_NOT_ENOUGH_ENERGY: number;
-declare const ERR_INVALID_TARGET: number;
-declare const ERR_FULL: number;
-declare const ERR_NOT_IN_RANGE: number;
-declare const ERR_INVALID_ARGS: number;
-declare const ERR_TIRED: number;
-declare const ERR_NO_BODYPART: number;
-declare const ERR_NOT_ENOUGH_EXTENSIONS: number;
-declare const ERR_RCL_NOT_ENOUGH: number;
-declare const ERR_GCL_NOT_ENOUGH: number;
+declare const TOP: 1;
+declare const TOP_RIGHT: 2;
+declare const RIGHT: 3;
+declare const BOTTOM_RIGHT: 4;
+declare const BOTTOM: 5;
+declare const BOTTOM_LEFT: 6;
+declare const LEFT: 7;
+declare const TOP_LEFT: 8;
 
-declare const COLOR_RED: number;
-declare const COLOR_PURPLE: number;
-declare const COLOR_BLUE: number;
-declare const COLOR_CYAN: number;
-declare const COLOR_GREEN: number;
-declare const COLOR_YELLOW: number;
-declare const COLOR_ORANGE: number;
-declare const COLOR_BROWN: number;
-declare const COLOR_GREY: number;
-declare const COLOR_WHITE: number;
+declare const COLOR_RED: 1;
+declare const COLOR_PURPLE: 2;
+declare const COLOR_BLUE: 3;
+declare const COLOR_CYAN: 4;
+declare const COLOR_GREEN: 5;
+declare const COLOR_YELLOW: 6;
+declare const COLOR_ORANGE: 7;
+declare const COLOR_BROWN: 8;
+declare const COLOR_GREY: 9;
+declare const COLOR_WHITE: 10;
 declare const COLORS_ALL: number[];
 
-declare const CREEP_SPAWN_TIME: number;
-declare const CREEP_LIFE_TIME: number;
-declare const CREEP_CLAIM_LIFE_TIME: number;
-declare const CREEP_CORPSE_RATE: number;
+declare const CREEP_SPAWN_TIME: 3;
+declare const CREEP_LIFE_TIME: 1500;
+declare const CREEP_CLAIM_LIFE_TIME: 500;
+declare const CREEP_CORPSE_RATE: 0.2;
 
 declare const OBSTACLE_OBJECT_TYPES: string[];
 
-declare const ENERGY_REGEN_TIME: number;
-declare const ENERGY_DECAY: number;
+declare const ENERGY_REGEN_TIME: 300;
+declare const ENERGY_DECAY: 1000;
 
-declare const REPAIR_COST: number;
+declare const REPAIR_COST: 0.01;
 
-declare const RAMPART_DECAY_AMOUNT: number;
-declare const RAMPART_DECAY_TIME: number;
-declare const RAMPART_HITS: number;
+declare const RAMPART_DECAY_AMOUNT: 300;
+declare const RAMPART_DECAY_TIME: 100;
+declare const RAMPART_HITS: 1;
 declare const RAMPART_HITS_MAX: {
-    2: number,
-    3: number,
-    4: number,
-    5: number,
-    6: number,
-    7: number,
-    8: number
+    2: 300000,
+    3: 1000000,
+    4: 3000000,
+    5: 10000000,
+    6: 30000000,
+    7: 100000000,
+    8: 300000000
 };
 
-declare const SPAWN_HITS: number;
-declare const SPAWN_ENERGY_START: number;
-declare const SPAWN_ENERGY_CAPACITY: number;
+declare const SPAWN_HITS: 5000;
+declare const SPAWN_ENERGY_START: 300;
+declare const SPAWN_ENERGY_CAPACITY: 300;
 
-declare const SOURCE_ENERGY_CAPACITY: number;
-declare const SOURCE_ENERGY_NEUTRAL_CAPACITY: number;
-declare const SOURCE_ENERGY_KEEPER_CAPACITY: number;
+declare const SOURCE_ENERGY_CAPACITY: 3000;
+declare const SOURCE_ENERGY_NEUTRAL_CAPACITY: 1500;
+declare const SOURCE_ENERGY_KEEPER_CAPACITY: 4000;
 
-declare const WALL_HITS: number;
-declare const WALL_HITS_MAX: number;
+declare const WALL_HITS: 1;
+declare const WALL_HITS_MAX: 300000000;
 
-declare const EXTENSION_HITS: number;
-declare const EXTENSION_ENERGY_CAPACITY: number;
+declare const EXTENSION_HITS: 1000;
+declare const EXTENSION_ENERGY_CAPACITY: {
+    0: 50,
+    1: 50,
+    2: 50,
+    3: 50,
+    4: 50,
+    5: 50,
+    6: 50,
+    7: 100,
+    8: 200
+};
 
-declare const ROAD_HITS: number;
-declare const ROAD_WEAROUT: number;
-declare const ROAD_DECAY_AMOUNT: number;
-declare const ROAD_DECAY_TIME: number;
+declare const ROAD_HITS: 5000;
+declare const ROAD_WEAROUT: 1;
+declare const ROAD_DECAY_AMOUNT: 100;
+declare const ROAD_DECAY_TIME: 1000;
 
-declare const LINK_HITS: number;
-declare const LINK_HITS_MAX: number;
-declare const LINK_CAPACITY: number;
-declare const LINK_COOLDOWN: number;
-declare const LINK_LOSS_RATION: number;
+declare const LINK_HITS: 1000;
+declare const LINK_HITS_MAX: 1000;
+declare const LINK_CAPACITY: 800;
+declare const LINK_COOLDOWN: 1;
+declare const LINK_LOSS_RATIO: 0.03;
 
-declare const STORAGE_CAPACITY: number;
-declare const STORAGE_HITS: number;
+declare const STORAGE_CAPACITY: 1000000;
+declare const STORAGE_HITS: 10000;
 
 declare const BODYPART_COST: {
     [part: string]: number;
-    move: number;
-    work: number;
-    attack: number;
-    carry: number;
-    heal: number;
-    ranged_attack: number;
-    tough: number;
-    claim: number;
+    move: 50;
+    work: 100;
+    attack: 80;
+    carry: 50;
+    heal: 250;
+    ranged_attack: 150;
+    tough: 10;
+    claim: 600;
 };
 declare const BODYPARTS_ALL: string[];
 
 
-declare const CARRY_CAPACITY: number;
-declare const HARVEST_POWER: number;
-declare const HARVEST_MINERAL_POWER: number;
-declare const REPAIR_POWER: number;
-declare const DISMANTLE_POWER: number;
-declare const BUILD_POWER: number;
-declare const ATTACK_POWER: number;
-declare const UPGRADE_CONTROLLER_POWER: number;
-declare const RANGED_ATTACK_POWER: number;
-declare const HEAL_POWER: number;
-declare const RANGED_HEAL_POWER: number;
-declare const DISMANTLE_COST: number;
+declare const CARRY_CAPACITY: 50;
+declare const HARVEST_POWER: 2;
+declare const HARVEST_MINERAL_POWER: 1;
+declare const REPAIR_POWER: 100;
+declare const DISMANTLE_POWER: 50;
+declare const BUILD_POWER: 5;
+declare const ATTACK_POWER: 30;
+declare const UPGRADE_CONTROLLER_POWER: 1;
+declare const RANGED_ATTACK_POWER: 10;
+declare const HEAL_POWER: 12;
+declare const RANGED_HEAL_POWER: 4;
+declare const DISMANTLE_COST: 0.005;
 
-declare const MOVE: string;
-declare const WORK: string;
-declare const CARRY: string;
-declare const ATTACK: string;
-declare const RANGED_ATTACK: string;
-declare const TOUGH: string;
-declare const HEAL: string;
-declare const CLAIM: string;
+declare const MOVE: "move";
+declare const WORK: "work";
+declare const CARRY: "carry";
+declare const ATTACK: "attack";
+declare const RANGED_ATTACK: "ranged_attack";
+declare const TOUGH: "tough";
+declare const HEAL: "heal";
+declare const CLAIM: "claim";
 
 declare const CONSTRUCTION_COST: {
-    spawn: number;
-    extension: number;
-    road: number;
-    constructedWall: number;
-    rampart: number;
-    link: number;
-    storage: number;
-    tower: number;
-    observer: number;
-    powerSpawn: number;
-    extractor: number;
-    lab: number;
-    terminal: number;
-    container: number;
-    nuker: number;
+    spawn: 15000,
+    extension: 3000,
+    road: 300,
+    constructedWall: 1,
+    rampart: 1,
+    link: 5000,
+    storage: 30000,
+    tower: 5000,
+    observer: 8000,
+    powerSpawn: 100000,
+    extractor: 5000,
+    lab: 50000,
+    terminal: 100000,
+    container: 5000,
+    nuker: 100000
 };
 
-declare const CONSTRUCTION_COST_ROAD_SWAMP_RATIO: number;
+declare const CONSTRUCTION_COST_ROAD_SWAMP_RATIO: 5;
 
-declare const STRUCTURE_EXTENSION: string;
-declare const STRUCTURE_RAMPART: string;
-declare const STRUCTURE_ROAD: string;
-declare const STRUCTURE_SPAWN: string;
-declare const STRUCTURE_LINK: string;
-declare const STRUCTURE_WALL: string;
-declare const STRUCTURE_KEEPER_LAIR: string;
-declare const STRUCTURE_CONTROLLER: string;
-declare const STRUCTURE_STORAGE: string;
-declare const STRUCTURE_TOWER: string;
-declare const STRUCTURE_OBSERVER: string;
-declare const STRUCTURE_POWER_BANK: string;
-declare const STRUCTURE_POWER_SPAWN: string;
-declare const STRUCTURE_EXTRACTOR: string;
-declare const STRUCTURE_LAB: string;
-declare const STRUCTURE_TERMINAL: string;
-declare const STRUCTURE_CONTAINER: string;
-declare const STRUCTURE_NUKER: string;
-declare const STRUCTURE_PORTAL: string;
+declare const STRUCTURE_EXTENSION: "extension";
+declare const STRUCTURE_RAMPART: "rampart";
+declare const STRUCTURE_ROAD: "road";
+declare const STRUCTURE_SPAWN: "spawn";
+declare const STRUCTURE_LINK: "link";
+declare const STRUCTURE_WALL: "wall";
+declare const STRUCTURE_KEEPER_LAIR: "keeperLair";
+declare const STRUCTURE_CONTROLLER: "controller";
+declare const STRUCTURE_STORAGE: "storage";
+declare const STRUCTURE_TOWER: "tower";
+declare const STRUCTURE_OBSERVER: "observer";
+declare const STRUCTURE_POWER_BANK: "powerBank";
+declare const STRUCTURE_POWER_SPAWN: "powerSpawn";
+declare const STRUCTURE_EXTRACTOR: "extractor";
+declare const STRUCTURE_LAB: "lab";
+declare const STRUCTURE_TERMINAL: "terminal";
+declare const STRUCTURE_CONTAINER: "container";
+declare const STRUCTURE_NUKER: "nuker";
+declare const STRUCTURE_PORTAL: "portal";
 
-declare const RESOURCE_ENERGY: string;
-declare const RESOURCE_POWER: string;
-declare const RESOURCE_UTRIUM: string;
-declare const RESOURCE_LEMERGIUM: string;
-declare const RESOURCE_KEANIUM: string;
-declare const RESOURCE_GHODIUM: string;
-declare const RESOURCE_ZYNTHIUM: string;
-declare const RESOURCE_OXYGEN: string;
-declare const RESOURCE_HYDROGEN: string;
-declare const RESOURCE_CATALYST: string;
-declare const RESOURCE_HYDROXIDE: string;
-declare const RESOURCE_ZYNTHIUM_KEANITE: string;
-declare const RESOURCE_UTRIUM_LEMERGITE: string;
-declare const RESOURCE_UTRIUM_HYDRIDE: string;
-declare const RESOURCE_UTRIUM_OXIDE: string;
-declare const RESOURCE_KEANIUM_HYDRIDE: string;
-declare const RESOURCE_KEANIUM_OXIDE: string;
-declare const RESOURCE_LEMERGIUM_HYDRIDE: string;
-declare const RESOURCE_LEMERGIUM_OXIDE: string;
-declare const RESOURCE_ZYNTHIUM_HYDRIDE: string;
-declare const RESOURCE_ZYNTHIUM_OXIDE: string;
-declare const RESOURCE_GHODIUM_HYDRIDE: string;
-declare const RESOURCE_GHODIUM_OXIDE: string;
-declare const RESOURCE_UTRIUM_ACID: string;
-declare const RESOURCE_UTRIUM_ALKALIDE: string;
-declare const RESOURCE_KEANIUM_ACID: string;
-declare const RESOURCE_KEANIUM_ALKALIDE: string;
-declare const RESOURCE_LEMERGIUM_ACID: string;
-declare const RESOURCE_LEMERGIUM_ALKALIDE: string;
-declare const RESOURCE_ZYNTHIUM_ACID: string;
-declare const RESOURCE_ZYNTHIUM_ALKALIDE: string;
-declare const RESOURCE_GHODIUM_ACID: string;
-declare const RESOURCE_GHODIUM_ALKALIDE: string;
-declare const RESOURCE_CATALYZED_UTRIUM_ACID: string;
-declare const RESOURCE_CATALYZED_UTRIUM_ALKALIDE: string;
-declare const RESOURCE_CATALYZED_KEANIUM_ACID: string;
-declare const RESOURCE_CATALYZED_KEANIUM_ALKALIDE: string;
-declare const RESOURCE_CATALYZED_LEMERGIUM_ACID: string;
-declare const RESOURCE_CATALYZED_LEMERGIUM_ALKALIDE: string;
-declare const RESOURCE_CATALYZED_ZYNTHIUM_ACID: string;
-declare const RESOURCE_CATALYZED_ZYNTHIUM_ALKALIDE: string;
-declare const RESOURCE_CATALYZED_GHODIUM_ACID: string;
-declare const RESOURCE_CATALYZED_GHODIUM_ALKALIDE: string;
+declare const RESOURCE_ENERGY: "energy";
+declare const RESOURCE_POWER: "power";
+declare const RESOURCE_UTRIUM: "U";
+declare const RESOURCE_LEMERGIUM: "L";
+declare const RESOURCE_KEANIUM: "K";
+declare const RESOURCE_GHODIUM: "G";
+declare const RESOURCE_ZYNTHIUM: "Z";
+declare const RESOURCE_OXYGEN: "O";
+declare const RESOURCE_HYDROGEN: "H";
+declare const RESOURCE_CATALYST: "X";
+declare const RESOURCE_HYDROXIDE: "OH";
+declare const RESOURCE_ZYNTHIUM_KEANITE: "ZK";
+declare const RESOURCE_UTRIUM_LEMERGITE: "UL";
+declare const RESOURCE_UTRIUM_HYDRIDE: "UH";
+declare const RESOURCE_UTRIUM_OXIDE: "UO";
+declare const RESOURCE_KEANIUM_HYDRIDE: "KH";
+declare const RESOURCE_KEANIUM_OXIDE: "KO";
+declare const RESOURCE_LEMERGIUM_HYDRIDE: "LH";
+declare const RESOURCE_LEMERGIUM_OXIDE: "LO";
+declare const RESOURCE_ZYNTHIUM_HYDRIDE: "ZH";
+declare const RESOURCE_ZYNTHIUM_OXIDE: "ZO";
+declare const RESOURCE_GHODIUM_HYDRIDE: "GH";
+declare const RESOURCE_GHODIUM_OXIDE: "GO";
+declare const RESOURCE_UTRIUM_ACID: "UH2O";
+declare const RESOURCE_UTRIUM_ALKALIDE: "UHO2";
+declare const RESOURCE_KEANIUM_ACID: "KH2O";
+declare const RESOURCE_KEANIUM_ALKALIDE: "KHO2";
+declare const RESOURCE_LEMERGIUM_ACID: "LH2O";
+declare const RESOURCE_LEMERGIUM_ALKALIDE: "LHO2";
+declare const RESOURCE_ZYNTHIUM_ACID: "ZH2O";
+declare const RESOURCE_ZYNTHIUM_ALKALIDE: "ZHO2";
+declare const RESOURCE_GHODIUM_ACID: "GH2O";
+declare const RESOURCE_GHODIUM_ALKALIDE: "GHO2";
+declare const RESOURCE_CATALYZED_UTRIUM_ACID: "XUH2O";
+declare const RESOURCE_CATALYZED_UTRIUM_ALKALIDE: "XUHO2";
+declare const RESOURCE_CATALYZED_KEANIUM_ACID: "XKH2O";
+declare const RESOURCE_CATALYZED_KEANIUM_ALKALIDE: "XKHO2";
+declare const RESOURCE_CATALYZED_LEMERGIUM_ACID: "XLH2O";
+declare const RESOURCE_CATALYZED_LEMERGIUM_ALKALIDE: "XLHO2";
+declare const RESOURCE_CATALYZED_ZYNTHIUM_ACID: "XZH2O";
+declare const RESOURCE_CATALYZED_ZYNTHIUM_ALKALIDE: "ZXHO2";
+declare const RESOURCE_CATALYZED_GHODIUM_ACID: "XGH2O";
+declare const RESOURCE_CATALYZED_GHODIUM_ALKALIDE: "XGHO2";
 declare const RESOURCES_ALL: string[];
 
 declare const SUBSCRIPTION_TOKEN: string;
@@ -369,16 +380,16 @@ declare const BOOSTS: {
     }
 }
 
-declare const LOOK_CREEPS: string;
-declare const LOOK_ENERGY: string;
-declare const LOOK_RESOURCES: string;
-declare const LOOK_SOURCES: string;
-declare const LOOK_MINERALS: string;
-declare const LOOK_STRUCTURES: string;
-declare const LOOK_FLAGS: string;
-declare const LOOK_CONSTRUCTION_SITES: string;
-declare const LOOK_NUKES: string;
-declare const LOOK_TERRAIN: string;
+declare const LOOK_CREEPS: "creep";
+declare const LOOK_ENERGY: "energy";
+declare const LOOK_RESOURCES: "resource";
+declare const LOOK_SOURCES: "source";
+declare const LOOK_MINERALS: "mineral";
+declare const LOOK_STRUCTURES: "structure";
+declare const LOOK_FLAGS: "flag";
+declare const LOOK_CONSTRUCTION_SITES: "constructionSite";
+declare const LOOK_NUKES: "nuke";
+declare const LOOK_TERRAIN: "terrain";
 
-declare const ORDER_SELL: string;
-declare const ORDER_BUY: string;
+declare const ORDER_SELL: "sell";
+declare const ORDER_BUY: "buy";
