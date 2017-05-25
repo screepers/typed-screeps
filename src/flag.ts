78c64175e4f5cbb159e05576ca3d4c3a3ec047b5
Bryan Becker <mrbryanbecker@gmail.com>
2017-05-25 12:59:01 +0800

    Flag typings finished

--- src/flag.ts
+++ src/flag.ts
@@ -3,11 +3,11 @@
  */
 interface Flag extends RoomObject {
     readonly prototype: Flag;
-    
+
     /**
      * Flag color. One of the following constants: COLOR_WHITE, COLOR_GREY, COLOR_RED, COLOR_PURPLE, COLOR_BLUE, COLOR_CYAN, COLOR_GREEN, COLOR_YELLOW, COLOR_ORANGE, COLOR_BROWN
      */
-    color: number;
+    color: ColorConstant;
     /**
      * A shorthand to Memory.flags[flag.name]. You can use it for quick access the flag's specific memory data object.
      */
@@ -19,37 +19,37 @@ interface Flag extends RoomObject {
     /**
      * Flag secondary color. One of the COLOR_* constants.
      */
-    secondaryColor: number;
+    secondaryColor: ColorConstant;
     /**
      * Remove the flag.
      * @returns Result Code: OK
      */
-    remove(): number;
+    remove(): OK;
     /**
      * Set new color of the flag.
      * @param color One of the following constants: COLOR_WHITE, COLOR_GREY, COLOR_RED, COLOR_PURPLE, COLOR_BLUE, COLOR_CYAN, COLOR_GREEN, COLOR_YELLOW, COLOR_ORANGE, COLOR_BROWN
      * @parma secondaryColor Secondary color of the flag. One of the COLOR_* constants.
      * @returns Result Code: OK, ERR_INVALID_ARGS
      */
-    setColor(color: number, secondaryColor?: number): number;
+    setColor(color: ColorConstant, secondaryColor?: ColorConstant): OK | ERR_INVALID_ARGS;
     /**
      * Set new position of the flag.
      * @param x The X position in the room.
      * @param y The Y position in the room.
      * @returns Result Code: OK, ERR_INVALID_TARGET
      */
-    setPosition(x: number,y: number): number;
+    setPosition(x: number,y: number): OK | ERR_INVALID_ARGS;
     /**
      * Set new position of the flag.
      * @param pos Can be a RoomPosition object or any object containing RoomPosition.
      * @returns Result Code: OK, ERR_INVALID_TARGET
      */
-    setPosition(pos: RoomPosition|{pos: RoomPosition}): number;
+    setPosition(pos: RoomPosition|{pos: RoomPosition}): OK | ERR_INVALID_ARGS;
 }
 
 interface FlagConstructor extends _Constructor<Flag> {
-    new (name: string, color: number, secondaryColor: number, roomName: string, x: number, y: number): Flag;
-    (name: string, color: number, secondaryColor: number, roomName: string, x: number, y: number): Flag;
+    new (name: string, color: ColorConstant, secondaryColor: ColorConstant, roomName: string, x: number, y: number): Flag;
+    (name: string, color: ColorConstant, secondaryColor: ColorConstant, roomName: string, x: number, y: number): Flag;
 }
 
 declare const Flag: FlagConstructor;
