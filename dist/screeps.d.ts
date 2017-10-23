6b7459798657f81d02eacffadbb88895245eec45
apemanzilla <dominic@marcuse.us>
2017-10-23 18:14:29 -0400

    Add StructureSpawn.spawnCreep

--- dist/screeps.d.ts
+++ dist/screeps.d.ts
@@ -2156,6 +2156,26 @@ interface StructureSpawn extends OwnedStructure<STRUCTURE_SPAWN> {
      */
     createCreep(body: BodyPartConstant[], name?: string, memory?: CreepMemory): ScreepsReturnCode | string;
     /**
+     * Start the creep spawning process. The required energy amount can be withdrawn from all spawns and extensions in the room.
+     * Returns one of the following:
+     * OK    0 The operation has been scheduled successfully.
+     * ERR_NOT_OWNER    -1 You are not the owner of this spawn.
+     * ERR_NAME_EXISTS    -3 There is a creep with the same name already.
+     * ERR_BUSY    -4 The spawn is already in process of spawning another creep.
+     * ERR_NOT_ENOUGH_ENERGY    -6 The spawn and its extensions contain not enough energy to create a creep with the given body.
+     * ERR_INVALID_ARGS    -10 Body is not properly described or name was not provided.
+     * ERR_RCL_NOT_ENOUGH    -14 Your Room Controller level is insufficient to use this spawn.
+     * @param {BodyPartConstant[]} body An array describing the new creep’s body. Should contain 1 to 50 elements with one of these constants:WORK, MOVE, CARRY, ATTACK, RANGED_ATTACK, HEAL, TOUGH, CLAIM
+     * @param {string} name The name of a new creep. It must be a unique creep name, i.e. the Game.creeps object should not contain another creep with the same name (hash key).
+     * @param {{memory?: CreepMemory; energyStructures?: (StructureSpawn | StructureExtension)[]; dryRun?: boolean}} opts An object with additional options for the spawning process.
+     * @returns {ScreepsReturnCode}
+     */
+    spawnCreep(body: BodyPartConstant[], name: string, opts?: {
+        memory?: CreepMemory;
+        energyStructures?: (StructureSpawn | StructureExtension)[];
+        dryRun?: boolean;
+    }): ScreepsReturnCode;
+    /**
      * Destroy this spawn immediately.
      */
     destroy(): ScreepsReturnCode;
