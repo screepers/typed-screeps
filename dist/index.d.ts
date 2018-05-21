bf0868b16d0c95077dc7fc5120ac0f9886595d76
pmoehl <p_moehl@gmx.de>
2018-05-21 20:11:54 +0200

    Tombstone doc from official api docs

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3899,15 +3899,37 @@ type AnyStructure =
     StructurePortal |
     StructureRoad |
     StructureWall;
+/**
+ * A remnant of dead creeps. This is a walkable structure.
+ * <ul>
+ *     <li>Decay: 5 ticks per body part of the deceased creep</li>
+ * </ul>
+ */
 interface Tombstone extends RoomObject {
-    /** The tombstones game objects id. */
+    /**
+     * A unique object identificator.
+     * You can use {@link Game.getObjectById} method to retrieve an object instance by its id.
+     */
     id: string;
-    /** The tick that the creep died. */
+    /**
+     * Time of death.
+     */
     deathTime: number;
+    /**
+     * An object with the tombstone contents.
+     * Each object key is one of the RESOURCE_* constants, values are resources amounts.
+     * RESOURCE_ENERGY is always defined and equals to 0 when empty,
+     * other resources are undefined when empty.
+     * You can use lodash.sum to get the total amount of contents.
+     */
     store: StoreDefinition;
-    /** How many ticks until this tombstone decays */
+    /**
+     * The amount of game ticks before this tombstone decays.
+     */
     ticksToDecay: number;
-    /** The creep that died to create this tombstone */
+    /**
+     * An object containing the deceased creep.
+     */
     creep: Creep;
 }
 
