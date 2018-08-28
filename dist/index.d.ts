775813ff9516a56fab9bdf011bb4db4372245cb7
Tim Perkins <tprk77@gmail.com>
2018-08-27 23:12:54 -0400

    Small fix to options of findInRange
    To be consistent with the other definition and "Room.find".

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2535,7 +2535,7 @@ interface RoomPosition {
      * @param range The range distance.
      * @param opts See Room.find.
      */
-    findInRange<K extends FindConstant>(type: K, range: number, opts?: {filter: any| string}): Array<FindTypes[K]>;
+    findInRange<K extends FindConstant>(type: K, range: number, opts?: {filter: FilterFunction<K>}): Array<FindTypes[K]>;
     findInRange<T extends Structure>(type: FIND_STRUCTURES | FIND_MY_STRUCTURES | FIND_HOSTILE_STRUCTURES, range: number, opts?: {filter: FilterFunction<FIND_STRUCTURES>}): T[];
     /**
      * Find all objects in the specified linear range.
