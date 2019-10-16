5f301968b065afb751394de7038eeba3605d34c0
Marcel Behrmann <mail@marcelbehrmann.de>
2019-10-16 20:46:38 +0200

    fix tslint errors

--- src/store.ts
+++ src/store.ts
@@ -4,17 +4,17 @@ interface Store extends Record<ResourceConstant, number> {
      * Returns capacity of this store for the specified resource, or total capacity if resource is undefined.
      * @param resource The type of the resource
      */
-    getCapacity(resource?: ResourceConstant): Number | null;
+    getCapacity(resource?: ResourceConstant): number | null;
 
     /**
      * A shorthand for getCapacity(resource) - getUsedCapacity(resource)
-     * @param resource
+     * @param resource The type of the resource
      */
-    getFreeCapacity(resource?: ResourceConstant): Number | null;
+    getFreeCapacity(resource?: ResourceConstant): number | null;
 
     /**
      * Returns the capacity used by the specified resource, or total used capacity for general purpose stores if resource is undefined.
      * @param resource The type of the resource.
      */
-    getUsedCapacity(resource?: ResourceConstant): Number | null;
+    getUsedCapacity(resource?: ResourceConstant): number | null;
 }
