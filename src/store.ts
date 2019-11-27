9ce0c1f57c06bfce8c4c0308410129a609804b44
Skyler Kehren <pyrodogg@gmail.com>
2019-11-27 08:45:45 +0200

    Fixes from 3.0 release
    - Update version to 3.0.1
    - Fix typo POSSIBLE_RESSOURCES -> POSSIBLE_RESOURCES in store definition
    - Remove unecessary tslint:disable rules in OpaqueTag definition

--- src/store.ts
+++ src/store.ts
@@ -1,26 +1,23 @@
-interface StoreBase<POSSIBLE_RESSOURCES extends ResourceConstant, UNLIMITED_STORE extends boolean> {
+interface StoreBase<POSSIBLE_RESOURCES extends ResourceConstant, UNLIMITED_STORE extends boolean> {
     /** Returns capacity of this store for the specified resource, or total capacity if resource is undefined. */
     getCapacity<R extends ResourceConstant | undefined>(
         resource?: R,
     ): UNLIMITED_STORE extends true
         ? null
         : (undefined extends R
-              ? (ResourceConstant extends POSSIBLE_RESSOURCES ? number : null)
-              : (R extends POSSIBLE_RESSOURCES ? number : null));
+              ? (ResourceConstant extends POSSIBLE_RESOURCES ? number : null)
+              : (R extends POSSIBLE_RESOURCES ? number : null));
     /** Returns the capacity used by the specified resource, or total used capacity for general purpose stores if resource is undefined. */
     getUsedCapacity<R extends ResourceConstant | undefined>(
         resource?: R,
-    ): undefined extends R ? (ResourceConstant extends POSSIBLE_RESSOURCES ? number : null) : (R extends POSSIBLE_RESSOURCES ? number : 0);
+    ): undefined extends R ? (ResourceConstant extends POSSIBLE_RESOURCES ? number : null) : (R extends POSSIBLE_RESOURCES ? number : 0);
     /** A shorthand for getCapacity(resource) - getUsedCapacity(resource). */
     getFreeCapacity(resource?: ResourceConstant): number;
 }
 
-type Store<POSSIBLE_RESSOURCES extends ResourceConstant, UNLIMITED_STORE extends boolean> = StoreBase<
-    POSSIBLE_RESSOURCES,
-    UNLIMITED_STORE
-> &
-    { [P in POSSIBLE_RESSOURCES]: number } &
-    { [P in Exclude<ResourceConstant, POSSIBLE_RESSOURCES>]: 0 };
+type Store<POSSIBLE_RESOURCES extends ResourceConstant, UNLIMITED_STORE extends boolean> = StoreBase<POSSIBLE_RESOURCES, UNLIMITED_STORE> &
+    { [P in POSSIBLE_RESOURCES]: number } &
+    { [P in Exclude<ResourceConstant, POSSIBLE_RESOURCES>]: 0 };
 
 interface GenericStoreBase {
     /** Returns capacity of this store for the specified resource, or total capacity if resource is undefined. */
