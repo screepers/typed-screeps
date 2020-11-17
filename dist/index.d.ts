a0e87cdb1960fa7953295cd9a702c41ff036c319
Skyler Kehren <pyrodogg@gmail.com>
2020-11-17 21:40:46 +0200

    Bump version 3.2.2-beta

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Screeps 3.2.1
+// Type definitions for Screeps 3.2.2-beta
 // Project: https://github.com/screeps/screeps
 // Definitions by: Marko Sulamägi <https://github.com/MarkoSulamagi>
 //                 Nhan Ho <https://github.com/NhanHo>
