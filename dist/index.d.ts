fea3f4455bf85686a8b691b37642e83447b25ca3
Skyler Kehren <pyrodogg@gmail.com>
2021-03-14 22:17:15 +0200

    Version 3.2.2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Screeps 3.2.2-beta
+// Type definitions for Screeps 3.2.2
 // Project: https://github.com/screeps/screeps
 // Definitions by: Marko Sulamägi <https://github.com/MarkoSulamagi>
 //                 Nhan Ho <https://github.com/NhanHo>
