8f73c99445a527673069c59c07263219b3f4d8a1
Mofeng <363034937@qq.com>
2023-02-16 16:40:49 +0800

    fix: brings `getObjectById(id: string)` back

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1571,6 +1571,15 @@ interface Game {
     getObjectById<T extends _HasId>(id: Id<T>): T | null;
 
     /**
+     * Get an object with the specified unique ID. It may be a game object of any type. Only objects from the rooms which are visible to you can be accessed.
+     * @param id The unique identifier.
+     * @returns an object instance or null if it cannot be found.
+     * @deprecated Use Id<T>, instead of strings, to increase type safety
+     */
+    // tslint:disable-next-line:unified-signatures
+    getObjectById<T extends _HasId>(id: string): T | null;
+
+    /**
      * Send a custom message at your profile email.
      *
      * This way, you can set up notifications to yourself on any occasion within the game.
