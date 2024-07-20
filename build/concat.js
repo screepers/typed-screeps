3c1b1b2d38580f8740eaf1a51212b924db9acb12
Mofeng <363034937@qq.com>
2024-07-20 23:19:57 +0800

    Adapt to new DefinitelyTyped layout
    * No longer needed to prepend header and descriptions

--- build/concat.js
+++ build/concat.js
@@ -1,16 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.readdir(path.join(__dirname, "..", "src"), function(err, files) {
-    files = files.map(function(value) {
+fs.readdir(path.join(__dirname, "..", "src"), function (err, files) {
+    files = files.map(function (value) {
         return path.join("src", value);
     });
 
-    Promise.all(files.map((name)=>fs.promises.readFile(name))).then(
-    (fileContents)=>{
-        fs.writeFileSync(path.join(__dirname, "..", "dist", "index.d.ts"),
-                         Buffer.concat(fileContents));
-    }, (reason)=>{
-        console.log(reason);
-    });
+    Promise.all(files.map((name) => fs.promises.readFile(name)))
+        .then((fileContents) => {
+            fs.writeFileSync(
+                path.join(__dirname, "..", "dist", "index.d.ts"),
+                Buffer.concat(fileContents)
+            );
+        }, (reason) => {
+            console.log(reason);
+        });
 });
