489e2ba39e3d0f48b4b6edb2bcc2acdea8d2ce0b
Etienne Samson <samson.etienne@gmail.com>
2025-01-31 11:32:40 +0100

    Format concat.js (#263)

--- build/concat.js
+++ build/concat.js
@@ -6,13 +6,12 @@ fs.readdir(path.join(__dirname, "..", "src"), function (err, files) {
         return path.join("src", value);
     });
 
-    Promise.all(files.map((name) => fs.promises.readFile(name)))
-        .then((fileContents) => {
-            fs.writeFileSync(
-                path.join(__dirname, "..", "dist", "index.d.ts"),
-                Buffer.concat(fileContents)
-            );
-        }, (reason) => {
+    Promise.all(files.map((name) => fs.promises.readFile(name))).then(
+        (fileContents) => {
+            fs.writeFileSync(path.join(__dirname, "..", "dist", "index.d.ts"), Buffer.concat(fileContents));
+        },
+        (reason) => {
             console.log(reason);
-        });
+        },
+    );
 });
